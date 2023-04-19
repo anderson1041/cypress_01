b12e20c8f6a3d32ee157ac5fdfee26083a66e307
Anderson Lima <anders0n1041@hotmail.com>
2023-04-18 23:40:23 -0300

    Selecionando o Login III

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,28 +1,24 @@
+//Automatização setando os componentes com Cypress
+
 describe('Login', () => {
   it('Tentativa de Login com user inexistente', () => {
     cy.visit('https://www.automationexercise.com/')
+    
     //seleciona um elemento com a escrita resgister now e clica
     cy.get('.shop-menu > .nav > :nth-child(4) > a').click();
-    cy.get('[data-qa="login-email"]').type('pedro.jenkins@email.com');
-    cy.get('[data-qa="login-password"]').type('123456789');
-    cy.get('[data-qa="login-button"]').click();
-    cy.get('.login-form > form > p').should('be.visible');
+    
+    //Seleciona o campo Login email e preenche com um email
+    cy.get('[data-qa="login-email"]').type('anderson.lima@email.com');
 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+    // Seleciona o campo senha e insere a senha
+    cy.get('[data-qa="login-password"]').type('123456789');
+
+    //Clica no botão para logar
+    cy.get('[data-qa="login-button"]').click();
+
+    // Aguarda a label surgir com a frase
+    cy.get('.login-form > form > p').should('be.visible').and('contain','Your email or password is incorrect!')
   
   })
+ 
 })
